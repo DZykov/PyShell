2a49c91505c7cd39c1e952b95fd5b3aeb9efadf7
DZykov <dzykov@protonmail.com>
2022-07-24 20:53:19 -0400

    add history class

--- history.py
+++ history.py
@@ -12,22 +12,30 @@ class History:
 
     def load_src(self):
         lines = []
-        with open(self.src) as f:
-            lines = f.readlines()
-        self.history = lines[:settings.HISTORY_MAX]
+        with open(self.src, 'r') as f:
+            for line in f:
+                self.history.insert(0, line[::-1])
     
     
-    def write_src(self, cmd):
-        pass
+    def write_src(self):
+        with open(self.src, 'w') as f:
+            for cmd in self.history:
+                f.write("%s\n" % cmd)
 
 
     def add(self, cmd):
-        pass
+        self.history.insert(0, cmd)
+        if len(self.history) == settings.HISTORY_MAX:
+            self.history.pop()
+        self.write_src()     
 
 
     def delete(self, cmd):
-        pass
+        self.history.pop()
+        self.write_src()
 
 
     def get_last(self):
-        pass
+        if len(self.history) > 0:
+            return self.history[0]
+        return None
